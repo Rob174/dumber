fc81edc9d91ea72c085bcc98af5cf66e39f94759
Rob174 <42274857+Rob174@users.noreply.github.com>
2021-04-09 14:57:40 +0100

    Update tasks.cpp

--- software/raspberry/superviseur-robot/tasks.cpp
+++ software/raspberry/superviseur-robot/tasks.cpp
@@ -324,6 +324,7 @@ void Tasks::ReceiveFromMonTask(void *arg) {
 
         if (msgRcv->CompareID(MESSAGE_MONITOR_LOST)) {
             delete(msgRcv);
+            cout << "Perte communication moniteur" << flush << endl;
             exit(-1);
         } else if (msgRcv->CompareID(MESSAGE_ROBOT_COM_OPEN)) {
             rt_sem_v(&sem_openComRobot);
